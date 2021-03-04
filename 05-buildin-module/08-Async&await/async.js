cc15f763d4626176694898bc5f16a3ddbb01f9fa
“hzx0728hzxtomnbv” <1964310665@qq.com>
2021-03-04 21:05:55 +0800

    scoket

--- 05-buildin-module/08-Async&await/async.js
+++ 05-buildin-module/08-Async&await/async.js
@@ -1,10 +1,25 @@
 let async = require('async');
 
 //串行无关联
-async.series([
-    (callback) => {
-        setTimeout(() => {
-            
-        },300)
+
+// console.time('test');
+// async.series([
+//     (callback) => {
+//         setTimeout(() => {
+//             callback(null,'one');
+//         },2000)
+//     },
+//     (callback) => {
+//         setTimeout(() => {
+//             callback(null,'two')
+//         },5000)
+//     }
+// ],(err,result) => {
+//     console.log(result);
+//     console.timeEnd('test');
+// })
+async.series({
+    one:(callback) => {
+        callback(null,'1');
     }
-])
+})
